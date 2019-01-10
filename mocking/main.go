878a0201a94348257b5f3fc9555c60be909b0084
Rosie Hamilton <enchantrix@gmail.com>
2019-01-10 07:41:58 +0000

    added some code that interacts with DynamoDB

--- mocking/main.go
+++ mocking/main.go
@@ -1,35 +1,34 @@
 package main
 
-import(
+import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-
 )
 
-type gopher struct{
-	ID string
-	name string
-	colour string
+type gopher struct {
+	ID     string `dynamodbav:"ID"`
+	Name   string `dynamodbav:"Name"`
+	Colour string `dynamodbav:"Colour"`
 }
 
 var dynamoSession dynamodbiface.DynamoDBAPI
 
-func main(){
+func main() {
 	awsConfig := aws.Config{Region: aws.String("eu-west-1")}
 	dynamoSession = dynamodb.New(session.New(&awsConfig))
 
-	gopher, err := getGopher("1")
+	gopher, err := getGopher("3")
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(gopher)
+	fmt.Println(tellStory(gopher))
 
 }
 
@@ -44,17 +43,22 @@ func getGopher(gopherID string) (gopher, error) {
 		TableName: aws.String("gophers"),
 	}
 
-
 	result, err := dynamoSession.GetItem(getItemInput)
 	if err != nil {
 		return gopher{}, err
 	}
 
 	gopher := gopher{}
-	if result.Item["ID"] != nil {
-		err = dynamodbattribute.UnmarshalMap(result.Item, &gopher)
-	}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &gopher)
 
 	return gopher, nil
 
+}
+
+func tellStory(g gopher) string {
+
+	story := fmt.Sprintf("Once upon a time there was a gopher called %s. Their fur was %s colour", g.Name, g.Colour)
+
+
+	return story
 }
