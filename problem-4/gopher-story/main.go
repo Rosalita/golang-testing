cbdb31afe5237d9002136e6ddd4018a7d2764621
Rosie Hamilton <enchantrix@gmail.com>
2019-02-18 13:26:45 +0000

    update examples

---
+++ problem-4/gopher-story/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+type gopher struct {
+	ID     string `dynamodbav:"ID"`
+	Name   string `dynamodbav:"Name"`
+	Colour string `dynamodbav:"Colour"`
+}
+
+var dynamoSession dynamodbiface.DynamoDBAPI
+var getRandomNum func(int) int
+
+func main() {
+	awsConfig := aws.Config{Region: aws.String("eu-west-1")}
+	dynamoSession = dynamodb.New(session.New(&awsConfig))
+	getRandomNum = getRandomNumFunc
+
+	err := randomStory()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func getGopher(gopherID string) (gopher, error) {
+
+	getItemInput := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String(gopherID),
+			},
+		},
+		TableName: aws.String("gophers"),
+	}
+
+	result, err := dynamoSession.GetItem(getItemInput)
+	if err != nil {
+		return gopher{}, err
+	}
+
+	gopher := gopher{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &gopher)
+
+	return gopher, nil
+
+}
+
+func tellStory(g gopher) string {
+	story := fmt.Sprintf("Once upon a time there was a gopher called %s. Their fur was %s colour", g.Name, g.Colour)
+	return story
+}
+
+func randomStory() error {
+
+	randNum := getRandomNum(4)
+	ID := strconv.Itoa(randNum)
+
+	gopher, err := getGopher(ID)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println(tellStory(gopher))
+
+	return nil
+
+}
+
+func getRandomNumFunc(x int) int {
+	rand.Seed(time.Now().UnixNano())
+	randNum := rand.Intn(x)
+	return randNum
+}
---
+++ problem-4/gopher-story/readme.md
@@ -0,0 +1,37 @@
+
+Problem 1  
+-----------  
+Function tellStory() has no tests  
+Examine input & output  
+Input is a gopher  
+Output is a string  
+Write some tests that feed gophers into tellStory()  
+Assert that stories about gophers are returned  
+  
+Problem 2  
+-----------  
+Function getGopher() gets a gopher from a real database  
+We want our test to run independently of that database  
+Mock the database using the dynamodbiface.DynamoDBAPI package  
+This will let the tests run independently of the real database.  
+  
+Problem 3  
+-----------  
+The randomStory() function gets a random story based on the time.  
+This means the result of randomStory() is not always going to be the same.  
+Mock the time so that randomStory() can be tested at a set moment in time.  
+
+Problem 4
+----------
+How can we test that something is logged?
+  
+Problem 5  
+-----------  
+The gopher-story code was not written TDD style.  
+Use TDD to write the code again.  
+
+Red / Green / Refactor tool
+https://github.com/nathany/looper
+
+Is it better?  
+Has it's testability improved?  
